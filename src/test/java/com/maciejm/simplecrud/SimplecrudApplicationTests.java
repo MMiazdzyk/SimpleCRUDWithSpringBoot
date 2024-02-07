8d96e2395a7ee3274acd7dd30cd3d75c0f1465a0
Maciej Miazdzyk <maciej.miazdzyk@merapar.com>
2024-02-07 14:27:30 +0100

    1. initial commit

---
+++ src/test/java/com/maciejm/simplecrud/SimplecrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.maciejm.simplecrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimplecrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
