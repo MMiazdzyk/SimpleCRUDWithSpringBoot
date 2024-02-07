cb0b0f55bff08b9faa31ad07cd28ba3c7c76b119
Maciej Miazdzyk <maciej.miazdzyk@merapar.com>
2024-02-07 17:38:44 +0100

    5. added general DAO interface

---
+++ src/main/java/com/maciejm/simplecrud/model/dao/DAO.java
@@ -0,0 +1,16 @@
+package com.maciejm.simplecrud.model.dao;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DAO <T>{
+    List<T> list();
+
+    T create(T t);
+
+    Optional<T> get(int id);
+
+    void update(T t, int id);
+
+    void delete(int id);
+}
