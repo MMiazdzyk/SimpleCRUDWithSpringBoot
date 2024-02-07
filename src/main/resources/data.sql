7bef4c85fe1288a1750fdb8111116ded8d94a96c
Maciej Miazdzyk <maciej.miazdzyk@merapar.com>
2024-02-07 17:18:18 +0100

    4. added DB schema and input data script

---
+++ src/main/resources/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO course(title, description, link) values('Java for beginers','Java for Beginners is a comprehensive introductory course designed to provide foundational knowledge and skills.','https://www.javaforbeginers.com');
+INSERT INTO course(title, description, link) values('Angular - advanced technics','This is the description for Angular advanced course','https://www.angularadv.io');
+INSERT INTO course(title, description, link) values('Java advanced technics','This is the description for Java advanced course','https://www.javaadvc.com.pl');
+INSERT INTO course(title, description, link) values('Better developer','Long and fantastic description for better developer course','https://wwww.bestdevever.org');
+INSERT INTO course(title, description, link) values('Cook your meal','a set of the best recipes and how to make them','https://mymeals.com');
