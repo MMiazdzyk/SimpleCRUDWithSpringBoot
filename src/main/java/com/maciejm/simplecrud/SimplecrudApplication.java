11128ce16ec6990baefd31e512e65200c74a520b
Maciej Miazdzyk <maciej.miazdzyk@merapar.com>
2024-02-07 21:32:43 +0100

    9. set up main method to list all courses using DAO

--- src/main/java/com/maciejm/simplecrud/SimplecrudApplication.java
+++ src/main/java/com/maciejm/simplecrud/SimplecrudApplication.java
@@ -1,13 +1,27 @@
 package com.maciejm.simplecrud;
 
+import com.maciejm.simplecrud.model.Course;
+import com.maciejm.simplecrud.model.dao.DAO;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.List;
 
 @SpringBootApplication
 public class SimplecrudApplication {
 
+	private static DAO<Course> dao;
+
+	public SimplecrudApplication(DAO<Course> dao) {
+		this.dao = dao;
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(SimplecrudApplication.class, args);
+
+		List<Course> courses = dao.list();
+		System.out.println("\n All courses -------------------------------------------- \n");
+		courses.forEach(System.out::println);
 	}
 
 }
