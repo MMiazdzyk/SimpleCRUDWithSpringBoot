647d923225040a48c4c380c75397449edfc9bb6a
Maciej Miazdzyk <maciej.miazdzyk@merapar.com>
2024-02-08 12:09:27 +0100

    16. added rest of the endpoints

--- src/main/java/com/maciejm/simplecrud/controller/CourseController.java
+++ src/main/java/com/maciejm/simplecrud/controller/CourseController.java
@@ -2,8 +2,15 @@ package com.maciejm.simplecrud.controller;
 
 import com.maciejm.simplecrud.dao.CourseDAO;
 import com.maciejm.simplecrud.model.Course;;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,10 +26,31 @@ public class CourseController {
     }
 
     @GetMapping("/")
+    @ResponseStatus(HttpStatus.OK)
     List<Course> findAll() {
         return courseServiceJdbcTemplate.list();
     }
 
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    Course findById(@PathVariable int id) {
+        return courseServiceJdbcTemplate.get(id).orElse(null);
+    }
 
+    @PostMapping("/")
+    @ResponseStatus(HttpStatus.CREATED)
+    void create(@RequestBody Course course) {
+        courseServiceJdbcTemplate.create(course);
+    }
 
+    @PutMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    void update(@RequestBody Course course, @PathVariable int id) {
+        courseServiceJdbcTemplate.update(course, id);
+    }
+
+    @DeleteMapping("/{id}")
+    void delete(@PathVariable int id) {
+        courseServiceJdbcTemplate.delete(id);
+    }
 }
