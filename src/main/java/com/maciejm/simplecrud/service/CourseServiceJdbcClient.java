04000ce41de70f38bec323a830a108295c3eea8a
Maciej Miazdzyk <maciej.miazdzyk@merapar.com>
2024-02-09 15:00:19 +0100

    21. implemented rest of crud methods with jdbc client

--- src/main/java/com/maciejm/simplecrud/service/CourseServiceJdbcClient.java
+++ src/main/java/com/maciejm/simplecrud/service/CourseServiceJdbcClient.java
@@ -26,21 +26,30 @@ public class CourseServiceJdbcClient implements CourseDAO {
 
     @Override
     public void create(Course course) {
-
+        jdbcClient.sql("INSERT INTO course(title, description, link) values(?,?,?)")
+                .params(List.of(course.getTitle(), course.getDescription(), course.getLink()))
+                .update();
     }
 
     @Override
     public Optional<Course> get(int id) {
-        return Optional.empty();
+        return jdbcClient.sql("SELECT course_id, title, description, link FROM course WHERE id = :id")
+                .param("id",id)
+                .query(Course.class)
+                .optional();
     }
 
     @Override
     public void update(Course course, int id) {
-
+        jdbcClient.sql("UPDATE course SET title = ?, description = ?, link = ? WHERE course_id = :id")
+                .param("id", id)
+                .update();
     }
 
     @Override
     public void delete(int id) {
-
+        jdbcClient.sql("DELETE FROM course WHERE course_id = :id")
+                .param("id", id)
+                .update();
     }
 }
